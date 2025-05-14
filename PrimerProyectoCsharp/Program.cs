8d38479249e434efd87e1594d88ffc934c3d430f
WinTux <rusok.software@gmail.com>
2025-05-13 20:13:24 -0400

    Agregar archivos de proyecto.

---
+++ PrimerProyectoCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimerProyectoCsharp
+{
+    internal class Program
+    {
+        public void Metodo1(string[] cosa) { 
+        
+        }
+        static int OtroMetodo() {
+            return 5;
+        }
+    }
+}
